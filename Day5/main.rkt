5deb849632eb8e32a4fa6414bef5ff261d44cba4
fluxehub <53355311+fluxehub@users.noreply.github.com>
2021-12-05 23:04:47 +0000

    Use regex instead

--- Day5/main.rkt
+++ Day5/main.rkt
@@ -1,75 +1,51 @@
 #lang racket
 
-(struct point (x y) 
-  #:transparent
-  #:methods gen:custom-write
-  [(define (write-proc point-val output-port output-mode)
-    (fprintf output-port "(~a, ~a)" (point-x point-val) (point-y point-val)))])
-
-(struct line (point0 point1)
-  #:transparent
-  #:methods gen:custom-write
-  [(define (write-proc line-val output-port output-mode)
-    (fprintf output-port "~a -> ~a" (line-point0 line-val) (line-point1 line-val)))])
-
-; I could have done regex but this works as well
-(define (parse-point str) 
-  (let* ([split (string-split str ",")]
-         [as-number (map string->number split)]) 
-    (apply point as-number)))
-
-; Returns a line as values x0 y0 x1 y1
-(define (unwrap-line line)
-  (let ([x0 (point-x (line-point0 line))]
-        [y0 (point-y (line-point0 line))]
-        [x1 (point-x (line-point1 line))]
-        [y1 (point-y (line-point1 line))])
-    (values x0 y0 x1 y1)))
+(define (p-dbg expr)
+  (begin
+    (displayln expr)
+    expr))
 
 (define (parse-line str)
-  (let* ([points (string-split str " -> ")]
-         [parsed (map parse-point points)]) 
-    (apply line parsed)))
+  (let ([parsed (car (regexp-match* #px"(\\d+),(\\d+) -> (\\d+),(\\d+)" str #:match-select cdr))])
+    (map string->number parsed)))
 
-(define (find-line-direction line)
-  (let-values ([(x0 y0 x1 y1) (unwrap-line line)])
-    (cond
-      [(= x0 x1) 'Vertical]
-      [(= y0 y1) 'Horizontal]
-      [else 'Angled])))
+(define (find-line-direction x0 y0 x1 y1)
+  (cond
+    [(= x0 x1) 'Vertical]
+    [(= y0 y1) 'Horizontal]
+    [else 'Angled]))
 
 ; Finds the points along a horizontal or vertical line
 ; Returns an empty list for lines that are not straight
-(define (points-along-line line)
-  (let-values ([(x0 y0 x1 y1) (unwrap-line line)])
-    (let ([direction (find-line-direction line)])
-      (case direction
-        ['Horizontal
-          (let ([start (min x0 x1)]
-                [end (+ 1 (max x0 x1))])
-            (for/list ([x (range start end)])
-              (point x y0)))]
-        ['Vertical
-          (let ([start (min y0 y1)]
-                [end (+ 1 (max y0 y1))])
-            (for/list ([y (range start end)])
-              (point x0 y)))]
-        [else '()]))))
+(define (points-along-line x0 y0 x1 y1)
+  (let ([direction (find-line-direction x0 y0 x1 y1)])
+    (case direction
+      ['Horizontal
+       (let ([start (min x0 x1)]
+             [end (+ 1 (max x0 x1))])
+         (for/list ([x (range start end)])
+           (list x y0)))]
+      ['Vertical
+       (let ([start (min y0 y1)]
+             [end (+ 1 (max y0 y1))])
+         (for/list ([y (range start end)])
+           (list x0 y)))]
+      [else '()])))
 
 (define (find-point-intersection-frequencies all-points)
   (let ([frequencies (make-hash)])
     (begin
       (for-each (lambda (points)
                   (for-each (lambda (point)
-                    (hash-set! frequencies point (+ 1 (hash-ref frequencies point 0))))
-                  points))
+                              (hash-set! frequencies point (+ 1 (hash-ref frequencies point 0))))
+                            points))
                 all-points)
-      
+
       frequencies)))
 
 (define (find-intersected-points input)
   (let* ([lines (map parse-line (file->lines input))]
-         [points (map points-along-line lines)]
+         [points (map (curry apply points-along-line) lines)]
          [frequencies (hash->list (find-point-intersection-frequencies points))])
     (filter (lambda (frequency) (>= (cdr frequency) 2)) frequencies)))
 
