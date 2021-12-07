281e7d3f4b649dd9dd4a8c9a8475d7651b2c1375
fluxehub <53355311+fluxehub@users.noreply.github.com>
2021-12-07 17:41:06 +0000

    Day 7 part 2

--- Day7/src/App.tsx
+++ Day7/src/App.tsx
@@ -1,36 +1,64 @@
 import React, { ChangeEventHandler, useState } from "react";
-import { Button, Stack, Heading, Textarea, Box, Text } from "@chakra-ui/react";
+import {
+  Button,
+  Stack,
+  Heading,
+  Textarea,
+  Box,
+  Text,
+  Flex,
+} from "@chakra-ui/react";
 
 interface Result {
   alignment: number;
   fuel: number;
 }
 interface ResultCardProps {
-  result: Result | undefined;
+  title: string;
+  result: Result;
+}
+
+interface ResultsProps {
+  titles: string[];
+  results: Result[] | undefined;
   ready: boolean;
 }
 
-const ResultCard = (props: ResultCardProps) => {
+const ResultCard = (props: ResultCardProps) => (
+  <Stack
+    boxShadow="lg"
+    bgColor="blue.500"
+    rounded={15}
+    p={4}
+    w="100%"
+    h="100%"
+    spacing={2}
+  >
+    <Heading as="h3" size="sm" color="white" fontWeight={600} opacity={0.7}>
+      {props.title}
+    </Heading>
+    <Heading as="h4" color="white" fontSize="4xl">
+      Best alignment: {props.result.alignment}
+    </Heading>
+    <Text color="white" opacity={0.9} fontWeight={600} fontSize="xl">
+      Fuel cost: {props.result.fuel}
+    </Text>
+  </Stack>
+);
+
+const Results = (props: ResultsProps) => {
   if (props.ready) {
     return (
-      <Box w={500} h={400} boxShadow="xl" bgColor="blue.500" rounded={15} p={4}>
-        <Text
-          as="h3"
-          size="sm"
-          mb={2}
-          color="white"
-          fontWeight={600}
-          opacity={0.7}
-        >
-          Submarine Alignment
-        </Text>
-        <Heading color="white" fontSize="4xl">
-          The most optimal horizontal position is {props.result!.alignment}.
-        </Heading>
-        <Text color="white" fontSize="xl" mt={2} opacity={0.8}>
-          This alignment costs {props.result!.fuel} fuel.
-        </Text>
-      </Box>
+      <Stack w={500} h={400}>
+        <ResultCard
+          title={props.titles[0]}
+          result={props.results![0]}
+        ></ResultCard>
+        <ResultCard
+          title={props.titles[1]}
+          result={props.results![1]}
+        ></ResultCard>
+      </Stack>
     );
   } else {
     return (
@@ -46,32 +74,47 @@ const ResultCard = (props: ResultCardProps) => {
 function App() {
   const [input, setInput] = useState("");
   const [ready, setReady] = useState(false);
-  const [result, setResult] = useState<Result>();
+  const [results, setResults] = useState<Result[]>();
 
   const calculate = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     const positions = input.split(",").map(parseFloat);
 
-    // Create an object to store the best alignment and fuel cost
-    let best: Result = { alignment: 0, fuel: Infinity };
+    let minAlignment = Math.min(...positions);
+    let maxAlignment = Math.max(...positions);
+
+    // Create an object to store the best alignment and fuel cost for part 1 and 2
+    let partOneBest: Result = { alignment: 0, fuel: Infinity };
+    let partTwoBest: Result = { alignment: 0, fuel: Infinity };
 
     // Iterate over each position and calculate the total fuel cost
-    for (var alignment of positions) {
-      var fuel = 0;
+    for (let i = minAlignment; i <= maxAlignment; i++) {
+      var partOneFuel = 0;
+      var partTwoFuel = 0;
 
       for (var pos of positions) {
-        fuel += Math.abs(pos - alignment);
+        const distance = Math.abs(pos - i);
+
+        // Part 1 fuel burn is the distance between the current position and the alignment position
+        partOneFuel += distance;
+
+        // Part 2 fuel burn is the sum of positions between the current position and the alignment position
+        partTwoFuel += (distance * (distance + 1)) / 2;
       }
 
       // If the total fuel cost is less than the current best, update the best
-      if (fuel < best.fuel) {
-        best = { alignment, fuel };
+      if (partOneFuel < partOneBest.fuel) {
+        partOneBest = { alignment: i, fuel: partOneFuel };
+      }
+
+      if (partTwoFuel < partTwoBest.fuel) {
+        partTwoBest = { alignment, fuel: partTwoFuel };
       }
     }
 
     // Update the result state
-    setResult(best);
+    setResults([partOneBest, partTwoBest]);
     setReady(true);
   };
 
@@ -105,11 +148,15 @@ function App() {
             </Stack>
           </form>
         </Stack>
-        <Stack>
-          <Heading as="h2" size="md" mb={2}>
+        <Stack spacing={4}>
+          <Heading as="h2" size="md">
             Result
           </Heading>
-          <ResultCard result={result} ready={ready} />
+          <Results
+            titles={["Part 1 - Linear Burn", "Part 2 - Non-Linear Burn"]}
+            results={results}
+            ready={ready}
+          />
         </Stack>
       </Stack>
     </Stack>
